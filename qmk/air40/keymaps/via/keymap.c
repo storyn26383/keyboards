06e9fa290300ce2212034fac9a5b351777f4b3db
Sasaya <storyn26383@gmail.com>
2025-06-30 22:41:26 +0800

    feat: modify keymap

--- qmk/air40/keymaps/via/keymap.c
+++ qmk/air40/keymaps/via/keymap.c
@@ -89,21 +89,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F6,        KC_MPLY, KC_LALT, KC_LGUI, LT(1, KC_SPC),                   LT(2, KC_SPC), KC_ENT,  KC_BSPC, KC_VOLD, KC_VOLU
     ),
     [_SYMBOL] = LAYOUT_ortho_4x12_2x2u(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_QUOT,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
-        _______, _______, _______, _______, KC_SPC,                    KC_SPC,  _______, _______, _______, _______
+        KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,   KC_5,  KC_6,  KC_7,   KC_8,  KC_9,  KC_0,    KC_EQL,
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,   KC_RSFT,
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_SPC,               KC_SPC, KC_NO, KC_NO, KC_NO,   KC_NO
     ),
     [_FUNCTION] = LAYOUT_ortho_4x12_2x2u(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
-        KC_MRWD, KC_MFFD, _______, QK_BOOT, KC_SPC,                    KC_SPC,  KC_ESC,  KC_CAPS, KC_BRID, KC_BRIU
+        KC_GRV,  KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_6,    KC_7,     KC_8,   KC_9,    KC_0,    KC_EQL,
+        KC_NO,   KC_NO,   RGB_RMOD, RGB_MOD,  RGB_TOG,  KC_NO, KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, KC_SCLN, KC_QUOT,
+        KC_LSFT, RGB_SPD, RGB_SPI,  RGB_VAD,  RGB_VAI,  KC_NO, KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_RSFT,
+        KC_MRWD, KC_MFFD, KC_NO,    QK_BOOT,  KC_SPC,                   KC_SPC,   KC_ESC, KC_CAPS, KC_BRID, KC_BRIU
     ),
     [_READLINE] = LAYOUT_ortho_4x12_2x2u(
-        _______,  _______,   _M_CTRL_W, _M_CTRL_E, _______,  _______, _______, _M_CTRL_U, _______, _______, KC_UP,   _______,
-        _______,  _M_CTRL_A, _______,   _______,   KC_RGHT, _______,  KC_BSPC, KC_ENT,    _______, _______, KC_ESC,  _______,
-        _______,  _______,   _______,   _______,   _______, KC_LEFT,  KC_DOWN, KC_ENT,    _______, _______, _______, _______,
-        _______,  _______,   _______,   _______,   _______,                    _______,   _______, _______, _______, _______
+        KC_NO, KC_NO,     _M_CTRL_W, _M_CTRL_E, KC_NO,   KC_NO,   KC_NO,   _M_CTRL_U, KC_NO, KC_NO, KC_UP,  KC_NO,
+        KC_NO, _M_CTRL_A, KC_NO,     KC_NO,     KC_RGHT, KC_NO,   KC_BSPC, KC_ENT,    KC_NO, KC_NO, KC_ESC, KC_NO,
+        KC_NO, KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_LEFT, KC_DOWN, KC_ENT,    KC_NO, KC_NO, KC_NO,  KC_NO,
+        KC_NO, KC_NO,     KC_NO,     KC_NO,     KC_NO,                     KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO
     )
 };
