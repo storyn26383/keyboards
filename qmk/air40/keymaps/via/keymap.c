e19c25c27734c4f6ac7ba0b446f199cbd7742c11
Sasaya <storyn26383@gmail.com>
2025-06-30 20:24:13 +0800

    feat: keymap

--- qmk/air40/keymaps/via/keymap.c
+++ qmk/air40/keymaps/via/keymap.c
@@ -1,55 +1,84 @@
-/* Copyright 2022 Dennis Kruyt (dennis@kruyt.org)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 enum layer_names {
   _QWERTY,
-  _LOWER,
-  _RAISE,
-  _ADJUST
+  _SYMBOL,
+  _FUNCTION,
+  _READLINE
 };
 
+enum macros {
+  _M_CTRL_A = SAFE_RANGE,
+  _M_CTRL_E,
+  _M_CTRL_W,
+  _M_CTRL_U
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!record->event.pressed) {
+    return true;
+  }
+
+  switch (keycode) {
+    case _M_CTRL_A:
+      register_code(KC_LGUI);
+      register_code(KC_LEFT);
+      unregister_code(KC_LEFT);
+      unregister_code(KC_LGUI);
+      return false;
+
+    case _M_CTRL_E:
+      register_code(KC_LGUI);
+      register_code(KC_RGHT);
+      unregister_code(KC_RGHT);
+      unregister_code(KC_LGUI);
+      return false;
+
+    case _M_CTRL_W:
+      register_code(KC_LALT);
+      register_code(KC_DEL);
+      unregister_code(KC_DEL);
+      unregister_code(KC_LALT);
+      return false;
+
+    case _M_CTRL_U:
+      register_code(KC_LGUI);
+      register_code(KC_DEL);
+      unregister_code(KC_DEL);
+      unregister_code(KC_LGUI);
+      return false;
+  }
+
+  return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_UP,   KC_LEFT, KC_DOWN, KC_RGHT
+    [_QWERTY] = LAYOUT_ortho_4x12_2x2u(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,    KC_MINS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    KC_LBRC, MT(MOD_LCTL, KC_RBRC),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_BSLS),
+        KC_F6,   KC_MPLY, KC_LALT, KC_LGUI, LT(1, KC_SPC),                   LT(2, KC_SPC), KC_ENT,  KC_BSPC, KC_VOLD, KC_VOLU
     ),
-    [_LOWER] = LAYOUT_ortho_4x12(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    [_SYMBOL] = LAYOUT_ortho_4x12_2x2u(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_QUOT,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
+        _______, _______, _______, _______, KC_SPC,                    KC_SPC,  _______, _______, _______, _______
     ),
-    [_RAISE] = LAYOUT_ortho_4x12(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    [_FUNCTION] = LAYOUT_ortho_4x12_2x2u(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
+        KC_MRWD, KC_MFFD, _______, QK_BOOT, KC_SPC,                    KC_SPC,  KC_ESC,  KC_CAPS, KC_BRID, KC_BRIU
     ),
-    [_ADJUST] = LAYOUT_ortho_4x12(
-        QK_BOOT, RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, _______, _______, _______, _______, _______, _______, DB_TOGG,
-        _______, RM_NEXT, RM_HUED, RM_SATD, RM_VALD, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    [_READLINE] = LAYOUT_ortho_4x12_2x2u(
+        _______,  _______,   _M_CTRL_W, _M_CTRL_E, _______,  _______, _______, _M_CTRL_U, _______, _______, KC_UP,   _______,
+        _______,  _M_CTRL_A, _______,   _______,   KC_RGHT, _______,  KC_BSPC, KC_ENT,    _______, _______, KC_ESC,  _______,
+        _______,  _______,   _______,   _______,   _______, KC_LEFT,  KC_DOWN, KC_ENT,    _______, _______, _______, _______,
+        _______,  _______,   _______,   _______,   _______,                    _______,   _______, _______, _______, _______
     )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _SYMBOL, _FUNCTION, _READLINE);
 }
